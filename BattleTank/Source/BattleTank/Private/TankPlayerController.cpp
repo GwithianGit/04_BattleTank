be179c32c1f99f45025fd96687c74bbabe58b4d9
Gwithian <gwithkol@gmail.com>
2017-11-12 12:43:57 +0000

    Lesson 122 - Checking and logging tank possesion

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -8,3 +8,17 @@ ATank* ATankPlayerController::GetControlledTank() const
 }
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing %s"), *(ControlledTank->GetName()));
+	}
+}
