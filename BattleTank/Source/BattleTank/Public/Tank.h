1f110b2fe5e2117d9336447565f2026612d7c86f
Gwithian <gwithkol@gmail.com>
2017-11-18 19:08:35 +0000

    Lesson 134 - Setting the barrel

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -18,6 +18,9 @@ class BATTLETANK_API ATank : public APawn
 public:
 	void AimAt(FVector OutHitLocation);
 
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
 
